3de25d07d9996f0b38a874bd62edf2c60bec15cd
Devraj sahani <sahani_devraj@srmap.edu.in>
2025-07-07 17:16:04 +0530

    video 7 changes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import express from "express"
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 const app= express()
+app.use(corsP({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true,
+}))
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended: true}))
 
 
 export { app }
