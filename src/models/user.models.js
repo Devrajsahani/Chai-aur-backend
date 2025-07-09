9a8b5d64bd084f6ebc8e7fbb4c1d49b5a5dc7589
Devraj sahani <sahani_devraj@srmap.edu.in>
2025-07-09 21:30:33 +0530

    user, jwt and models

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose,{schema}from"mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 const userschema=new Schema({
         username:{
             type:String,
@@ -44,6 +46,29 @@ const userschema=new Schema({
     
 })
 
+userSchema.pre("save", async function (next){
+    if(this.isModified("password"))return next();
+    // so here we are wriitng an extra line of code for checking that if we are updating any other field then our password field is not getting updated time to time, so we used if statement. and then after we wil call next 
+    this.password = bcrypt.hash(this.password, 8)
+    next()
+}) // befoer the data getting saved this will run and 
+
+userSchema.methods.generateAccessToken = function(){ // HERE WE ARE WATCHING HOW TO USE THE JWT TOKEN
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expriresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+userSchema.methods.generateRefreshToken= function(){}
 
 
 export const user = mongoose.model("User",userschema)
