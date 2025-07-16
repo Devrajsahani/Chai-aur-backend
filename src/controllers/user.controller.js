7e623f70245bf2ed9f933d4cde2b59123be68cc5
Devraj Sahani <sahani_devraj@srmap.edu.in>
2025-07-16 23:42:01 +0530

    revision of register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,49 @@
 import {asyncHandler} from "../utils/asynchandler";
-
+import {Apierror} from "./utils/apierror.js"
+import{user, User} from "../models/user.models.js"
 
 const registerUser = asyncHandler(async(req, res )=>{
-    res.status(200),json({
+    res.status(200).json({
         message:"ok"
     })
 })
 
+const {fullName,email,username,password} = req.body // here all the things are written that we need to extract,
+console.log("email:",email);
+if(fullName===""){
+    throw new Apierror(400,"full name is required")
+}
+// using this we can check that if all the elements are there in the fields or not, 
+if (
+    [fullName,email, password, username].some((field))=>
+        field?.trim()==="")
+{
+    throw new Apierror(400, "All the fields are required")
+}
+
+User.findOne({
+    $or:[{username},{password},{}] // here we can use any number of object here, 
+})
+if (existedUser) {
+    throw new Apierror(400,"User already exists!")
+    
+}
+
+const createdUser = await User.findById(user._id).select(
+    "-password -RefreshToken"
+)
+if(!createdUser){
+    throw Apierrorerror (400, "User not found ! ")
+}
+return res.status(201).json{
+    new Apiresponse(200, creadtedUser,"user is resgistered successfully")
+    
+}
+
+const avatar = await uploadoncloudinary(avatar)
+// since we made thsi helper file we don't need to write the code for the res. body and vagera vagera, we can simply 
+// use if statement to reqord 
+// we got all the details fo the database from doing (req.body), now this data can come from anywhere 
+// it could come from the form website, app, request, from anywhere.
+// now to destructure the data that we have there we use const {}
 export {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,24 @@
 import {router} from "express"
-import { registerUser } from "../controllers/user.controller";
+import { registerUser } from "../controllers/user.controller.js";
 
-const router = router()
 
-router.route("/register").post(registerUser) 
+const route = router();
+// so basically this code is just to tell that before the values or the pictures going to the backend
+// it is visiting the middleware and what all things will visit that is also mentioned,
+// How to inject middleware,
+router.route("/register").post( // we are using fields here because there could be alot of data in multer
+    // and it accepts all the data in the form of array and fields store it like that, 
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+
+        },
+        {
+            name:"CoverImage",
+            maxCount: 1
+        }
+    ]),registerUser) // here we have seen how to spot things and how to changethe values 
+
 
 export default router;
